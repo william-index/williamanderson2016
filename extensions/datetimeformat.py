1f183f67543d8bd298bf1c60b5407742817af83a
William Anderson <thewilliamanderson@gmail.com>
2017-12-29 18:09:47 -0500

    cleanup and update for 2017 year end and projects

--- extensions/datetimeformat.py
+++ extensions/datetimeformat.py
@@ -1,6 +1,7 @@
 # /extensions/datetimeformat.py
 
 import jinja2
+from jinja2.ext import Extension
 from datetime import datetime
 
 def datetimeformat(value, format='%Y-%m-%dT00:00:00-05:00'):
@@ -11,7 +12,7 @@ def datetimenow(string):
     time = datetime.now().time()
     return time.strftime('%Y-%m-%dT%H:%M:%S-05:00')
 
-class Datetimeformat(jinja2.ext.Extension):
+class Datetimeformat(Extension):
     def __init__(self, environment):
         super(Datetimeformat, self).__init__(environment)
         environment.filters['datetimeformat'] = datetimeformat
